6215c430fc5c93e3d23a21b06639d18ae49cd6a5
Manas Bhatt <72430129+manastech2@users.noreply.github.com>
2021-10-03 19:46:24 +0530

    Add files via upload

---
+++ TowerSiege-1-main/TowerSiege-1-main/ground.js
@@ -0,0 +1,18 @@
+class Ground {
+    constructor() {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(450,390,900,20,options);
+
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      noStroke()
+      rectMode(CENTER);
+      fill(188,67.67);
+      rect(pos.x, pos.y,900,20);
+
+    }
+  };
